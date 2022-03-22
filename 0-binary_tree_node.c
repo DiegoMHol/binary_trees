f1dcb9901905c44e7bf6535ba581c0732776b9c4
Diego Monfort <diegomonfort12346@gmail.com>
2022-03-22 12:40:55 -0700

    [SEGMENTATION FAULT]

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - Function to create Binary tree node
+ * @parent: Pointer to parent node
+ * @value: Value to add to node
+ * Return: Pointer to new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->right = NULL;
+	new_node->left = NULL;
+	return (new_node);
+}
