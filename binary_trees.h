c84dc41887fda8f8e8017d5791fd07a7a3489c3c
Diego Monfort <diegomonfort12346@gmail.com>
2022-03-23 13:53:17 -0700

    [CHANGE STRUCT]

--- binary_trees.h
+++ binary_trees.h
@@ -13,13 +13,14 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 }binary_tree_t;
+
 
 /* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
