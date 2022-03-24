7c853241735d3e054b87042ca12fce8a6766ea00
Diego Monfort <diegomonfort12346@gmail.com>
2022-03-23 19:27:48 -0700

    [WRONG OUTPUT]

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - Function to know if is root or not
+ * @node: node to check
+ * Return: 1 is root 0 if not
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+
+		return (0);
+
+	if ((node->left) || (node->right))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
