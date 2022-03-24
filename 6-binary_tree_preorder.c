b2d0a2b0978a670bbe786ba61fbca642882408db
Diego Monfort <diegomonfort12346@gmail.com>
2022-03-23 19:41:27 -0700

    [WRRONG OUTPUT]

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - Function goes tree by pre-order
+ * @tree: tree
+ * @func: pointer to a func for each node
+ * Return: 0
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->right, func);
+	binary_tree_preorder(tree->left, func);
+}
